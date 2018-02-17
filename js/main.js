c5703bcb2f753f96729580baa3ed942b4abb8246
chrisjimenez <chrisajim@gmail.com>
2018-02-17 12:47:04 -0500

    update to script, humans.txt

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 (function(){
   var colors = [
-    "#3498db",
-    "#c0392b",
-    "#16a085",
-    "#2980b9",
-    "#2c3e50",
-    "#d35400",
-    "#e67e22"
+    '#3498db',
+    '#c0392b',
+    '#16a085',
+    '#2980b9',
+    '#2c3e50',
+    '#d35400',
+    '#e67e22'
   ];
 
   setInterval(function(){
     var color = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = color;
   }, 15000);
-
-})()
+})();
